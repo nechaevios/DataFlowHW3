da44629b785c8c5d932ebe808c38a6afc97377ef
Sergey <21218903+xchakiex@users.noreply.github.com>
2021-11-05 17:22:39 +0300

    Initial Commit

---
+++ DataFlowHW3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DataFlowHW3
+//
+//  Created by Nechaev Sergey  on 05.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DataFlowHW3/DataFlowHW3App.swift
@@ -0,0 +1,17 @@
+//
+//  DataFlowHW3App.swift
+//  DataFlowHW3
+//
+//  Created by Nechaev Sergey  on 05.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct DataFlowHW3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
